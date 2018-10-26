2a52554d0a3d52a1a2634b4819924b84bd86b691
Angelo Luz <angelogl@gmail.com>
2018-10-26 11:59:15 -0300

    Cria interface da lista simplesmente encadeada

--- src/br/com/senacrs/lista/Lista.java
+++ src/br/com/senacrs/lista/Lista.java
@@ -2,7 +2,7 @@ package br.com.senacrs.lista;
 
 import br.com.senacrs.exception.EmptyListException;
 
-public class Lista <T> {
+public class Lista <T> implements ListaSimplesmenteEncadeada<T> {
     private NodoSimples<T> primeiro;
     private NodoSimples<T> ultimo;
     private String nome;
@@ -15,26 +15,32 @@ public class Lista <T> {
         primeiro = ultimo = null;
     }
 
+    @Override
     public void insereNoFim(T item){
         //TODO: Desenvolver comportamento
     }
 
+    @Override
     public void insereNoInicio(T item){
         //TODO: Desenvolver comportamento
     }
 
-    public void visualizaLista(T item){
+    @Override
+    public void visualizaLista(){
         //TODO: Desenvolver comportamento
     }
+    @Override
     public T removeDoInicio() throws EmptyListException {
         //TODO: Desenvolver comportamento
         throw new UnsupportedOperationException("Implementar");
     }
+    @Override
     public T removeDoFim() throws EmptyListException {
         //TODO: Desenvolver comportamento
         throw new UnsupportedOperationException("Implementar");
     }
 
+    @Override
     public boolean isVazio(){
         //TODO: Desenvolver comportamento
         throw new UnsupportedOperationException("Implementar");
